a9128cb4e7a36d17719bb8b3f8e0a90562f2ef9a
Jun <junsoopark92@gmail.com>
2022-08-16 11:13:21 -0400

    blocks

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,21 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns {void}
+ */
+
 export function init(config) {
     return true
 }
+
+/**
+ * 
+ * @param {number} code 
+ * @returns {number} 
+ */
 export function exit(code) {
     return code + 1
 }
